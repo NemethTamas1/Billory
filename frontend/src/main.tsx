fe74590eb6ebff1738b8774a9878d0e3b8e16bd0
NemethTamas1 <nemethtamas029@gmail.com>
2025-10-20 16:07:22 +0200

    .env moved to the root directory. jsx turned into tsx.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import MainPage from "./pages/MainPage.jsx";
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from "./router/router.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router}/>
   </StrictMode>
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Clients from "../pages/Clients.jsx";
-import Financials from "../pages/Financials.jsx";
+import Financials from "../pages/Financials.js";
 import Invoices from "../pages/Invoices.jsx";
 import Products from "../pages/Products.jsx";
 import MainPage from "../pages/MainPage.jsx";
-import PageNotFound from "../pages/PageNotFound.jsx";
+import PageNotFound from "../pages/PageNotFound.js";
 
 const router = createBrowserRouter([
     {
