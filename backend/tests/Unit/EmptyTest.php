ebba85409c6856297d15486df66cadafc7214183
NemethTamas1 <nemethtamas029@gmail.com>
2025-10-27 13:44:59 +0100

    dummy test added so the CI workflow can finish

---
+++ backend/tests/Unit/EmptyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class EmptyTest extends TestCase
+{
+    public function test_dummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
